e88afdab5ff21112bc8f10870f98d19f2d3de25a
Taras Dutchak <Tarasdutchak90@gmail.com>
2019-12-01 11:54:41 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,55 @@
+"use strict";
+
+let a = 5;
+console.log(a);
+
+// var string = "Some text";
+// var number = 24;
+// var boolean = true;
+// var sumbol = Symbol();
+// null;
+// undefined;
+// var obj = {};
+
+
+// let peson = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(peson["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]);
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+
+
+
+// let answer = +prompt("Чи Вам є 18?", "Так");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+
+// console.log(4 + " - object");
+
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5 % 2);
+// console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
